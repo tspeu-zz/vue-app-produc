3f715d829188ef9945d0536b889174edfee9aa4d
JM_B <jmbanchero@gmail.com>
2018-12-13 11:57:28 +0000

    first VUE

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-
-var product= "Socks";
+//vue instance-> el #element bind to de div Id #
+//data->
+//actions->
+var app= new Vue({
+    el: '#app',
+    data : {
+        product: 'Socks | Calcetines'
+    }
+});
