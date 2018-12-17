f5c9c2b5f826d69ba7b9b8472b0f24395718e8e1
JM_B <jmbanchero@gmail.com>
2018-12-17 11:41:30 +0000

    add ++

--- main.js
+++ main.js
@@ -4,6 +4,19 @@
 var app= new Vue({
     el: '#app',
     data : {
-        product: 'Socks | Calcetines'
+        product: 'Socks',
+        linkin : false,
+        buttonDeshabi : true,
+        segundos : 0,
+        image: 'assets/sock-green.jpeg',
+        altText :'A pair of VUE socks',
+        anchText: '#',
+        inStock : true, 
+        inventory : 100
+    },
+    created(){
+        // setInterval(() => {
+        //     this.segundos ++
+        // } , 1000);
     }
 });
